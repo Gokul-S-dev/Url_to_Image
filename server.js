53c2c34dfba2e5b61dc60f12a5c5ba4bd3272335
gokul-12-abc <sggokul762@gmail.com>
2025-08-23 22:05:59 +0530

    Final commit:Complete fullstack project developed

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const path = require('path')
+const axios = require('axios')
+const fs = require('fs');
 
 const app = express();
 const PORT = 3000;
@@ -9,12 +11,48 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname,"public")));
 
-app.post('/download',(req,res) => {
-    const url = req.body.url;
-    console.log(`URL: ${url}`);
-    
-    // Send a response back to the browser
-    res.send(`Received URL: ${url}`);
+app.post('/download',async(req,res) => {
+    try{
+    const imgurl = req.body.url;
+    console.log(`URL: ${imgurl}`);
+  
+
+    //fetch image using axios
+    const response = await axios({
+       url: imgurl,
+       method:"GET",
+       responseType:"stream",
+    });
+
+    //Create unique filename
+    const fileName=`img_${Date.now()}.jpg`;
+    const savePath= path.join("downloads",fileName);
+
+    //Make sure folder exists
+    if(!fs.existsSync("downloads")){
+        fs.mkdirSync("downloads");
+    }
+
+
+    //Save image file
+    const writer = fs.createWriteStream(savePath);
+    response.data.pipe(writer);
+
+    writer.on("finish", ()=>{
+        res.json({
+            message:"Image download",
+            file:fileName
+        });
+    });
+
+    writer.on("error",(err) =>{
+        res.status(500).json({error:"Erroe saving image",details:err.message})
+    })
+      
+}
+catch(error){
+  res.status(500).json({error:"Failed to download image",details:error.message})
+}
 })
 
 app.listen(PORT, () => {
